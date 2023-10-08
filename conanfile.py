fb4cfedf2423fb72c3a90e7599ff0804d66c91a8
Josh Gustafson <j@gustafson.me>
2023-10-07 18:38:11 -0600

    Conan 2

--- conanfile.py
+++ conanfile.py
@@ -1,17 +1,20 @@
-from conans import ConanFile, CMake, tools
-
+from conan import ConanFile
+from conan.tools.cmake import CMake
+from conan.tools import files
+from os.path import join
 
 class LibConan(ConanFile):
-    name = "ZoneDetect"
+    name = "zonedetect"
     version = "1.0"
     options = {"shared": [True, False]}
     default_options = {"shared": False}
-    settings = [ "os", "compiler", "build_type", "arch", "cppstd" ]
+    settings = ["os", "compiler", "build_type", "arch"]
     requires = [
         "shapelib/1.5.0",
         "gtest/1.11.0",
     ]
-    generators = "cmake_find_package"
+    tool_requires = "cmake/3.27.6"
+    generators = "CMakeToolchain", "CMakeDeps"
     exports_sources = "*"
 
     def build(self):
@@ -21,14 +24,14 @@ class LibConan(ConanFile):
         cmake.build()
 
     def package(self):
-        self.copy("*.h", dst="include", src="library")
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.dylib*", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
-        self.copy("timezone21.bin", dst="include", src="out_v1", keep_path=False)
+        files.copy(self, "*.h", join(self.source_folder, "library"), join(self.package_folder, "include"))
+        files.copy(self, "*.lib", self.build_folder, join(self.package_folder, "lib"), keep_path=False)
+        files.copy(self, "*.dll", self.build_folder, join(self.package_folder, "bin"), keep_path=False)
+        files.copy(self, "*.dylib*", self.build_folder, join(self.package_folder, "lib"), keep_path=False)
+        files.copy(self, "*.so", self.build_folder, join(self.package_folder, "lib"), keep_path=False)
+        files.copy(self, "*.a", self.build_folder, join(self.package_folder, "lib"), keep_path=False)
+        files.copy(self, "timezone21.bin", join(self.build_folder, "out_v1"), join(self.package_folder, "include"), keep_path=False)
 
     def package_info(self):
         self.cpp_info.includedirs = ['include']
-        self.cpp_info.libs = [ "zonedetect" ]
+        self.cpp_info.libs = ["zonedetect"]
